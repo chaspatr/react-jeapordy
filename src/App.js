1904b4af2e0268d76bb9b520c21ca501f9ef31cc
Chase Patrick <chasepatrick117@gmail.com>
2020-08-20 23:33:48 -0400

    added dynamic routing with a switch component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Welcome from "./components/welcome/Welcome";
+import Clock from "./components/clock/Clock";
+import Contact from "./components/contact/Contact";
+import NoMatch from "./components/no-match/NoMatch";
+import Navigation from "./components/navigation/Navigation";
+import { Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(props) => <Welcome {...props} name="Chase" />}
+          />
+          <Route
+            path="/welcome/:name"
+            render={(props) => (
+              <Welcome {...props} name={props.match.params.name} />
+            )}
+          />
+          <Route path="/clock" component={Clock} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/no-match/NoMatch.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NoMatch(props) {
+  return (
+    <div>
+      <div>Error 404</div>
+      <br />
+      <div>No match was found for the specified URL</div>
+    </div>
+  );
+}
+
+export default NoMatch;
