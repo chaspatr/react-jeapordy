5bc0e36e3f0ea65ea35aae181f4433eaccfb419e
Chase Patrick <chasepatrick117@gmail.com>
2020-09-15 21:04:24 -0400

    Updates

---
+++ precache-manifest.d4cd69953ee86d94331e9e0616894ea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa8e9a14bf2eebf86e39b8be468e6a2d",
+    "url": "/react-jeopardy/index.html"
+  },
+  {
+    "revision": "d320fb80685d97766fda",
+    "url": "/react-jeopardy/static/css/main.be521adc.chunk.css"
+  },
+  {
+    "revision": "52823c1948e90b0c163b",
+    "url": "/react-jeopardy/static/js/2.e9e4cedb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-jeopardy/static/js/2.e9e4cedb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d320fb80685d97766fda",
+    "url": "/react-jeopardy/static/js/main.8ee7d3ef.chunk.js"
+  },
+  {
+    "revision": "1bc2529b99d69a2d882c",
+    "url": "/react-jeopardy/static/js/runtime-main.d08202c7.js"
+  }
+]);
